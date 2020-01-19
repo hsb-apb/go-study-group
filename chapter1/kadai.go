907206f4bba482bdb03ba594173f98452c460679
Hasebe Takumi <hasebe_takumi@applibot.co.jp>
2020-01-19 18:06:02 +0900

    課題終了

--- chapter1/kadai.go
+++ chapter1/kadai.go
@@ -1,8 +1,12 @@
 package chapter1
 
 import (
-	"chapter1/lib"
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/apbgo/go-study-group/chapter1/lib"
 )
 
 // Calc opには+,-,×,÷の4つが渡ってくることを想定してxとyについて計算して返却(正常時はerrorはnilでよい)
@@ -48,10 +52,23 @@ func Sqrt(x float64) float64 {
 
 	// TODO Q3
 	z := 1.0
-	for z < 0.00000000000005 {
+	for !isApproximation(z*z, x) {
 		z -= (z*z - x) / (2 * z)
 	}
 	return z
+}
+
+// 差が0.00000000000005以下ならtrueを返却する
+func isApproximation(x, y float64) bool {
+	difference := x - y
+	if difference < 0 {
+		difference *= -1
+	}
+	if difference < 0.00000000000005 {
+		// 差が既定値以下
+		return true
+	}
+	return false
 }
 
 // Pyramid x段のピラミッドを文字列にして返却
@@ -62,8 +79,22 @@ func Pyramid(x int) string {
 	// int -> stringはstrconv.Ioa() https://golang.org/pkg/strconv/#Itoa
 
 	// TODO Q4
+	if x <= 0 {
+		// 0以下はerrorを返却
+		return "error"
+	}
+	var result string = ""
+	for i := 1; i <= x; i++ {
+		if i != 1 {
+			// 2以上のときに改行つける
+			result += "\n"
+		}
+		for j := 1; j <= i; j++ {
+			result += strconv.Itoa(j)
+		}
+	}
 
-	return ""
+	return result
 }
 
 // StringSum x,yをintにキャストし合計値を返却 (正常終了時、errorはnilでよい)
@@ -74,8 +105,15 @@ func StringSum(x, y string) (int, error) {
 	// string -> intはstrconv.Atoi() https://golang.org/pkg/strconv/#Atoi
 
 	// TODO Q5
-
-	return 0, nil
+	iX, err := strconv.Atoi(x)
+	if err != nil {
+		return 0, err
+	}
+	iY, err := strconv.Atoi(y)
+	if err != nil {
+		return 0, err
+	}
+	return iX + iY, nil
 }
 
 // SumFromFileNumber ファイルを開いてそこに記載のある数字の和を返却
@@ -84,6 +122,25 @@ func SumFromFileNumber(filePath string) (int, error) {
 	// bufio.Scannerなどで１行ずつ読み込むと良い
 
 	// TODO Q6 オプション
-
-	return 0, nil
+	file, err := os.Open(filePath)
+	if err != nil {
+		// 開けない
+		return 0, err
+	}
+	defer file.Close()
+	result := 0
+	sc := bufio.NewScanner(file)
+	for i := 1; sc.Scan(); i++ {
+		if err := sc.Err(); err != nil {
+			// エラー処理
+			return 0, err
+		}
+		rowVal, err := strconv.Atoi(sc.Text())
+		if err != nil {
+			// 数字じゃなかったら飛ばす
+			continue
+		}
+		result += rowVal
+	}
+	return result, nil
 }
