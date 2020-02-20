34691fbb2a1cb945e2dfbe9727d9aa1e5fcd6893
Hasebe Takumi <hasebe_takumi@applibot.co.jp>
2020-02-21 01:32:02 +0900

    Chapter4提出

--- chapter4/main.go
+++ chapter4/main.go
@@ -1,6 +1,40 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var delimiter = flag.String("d", ",", "区切り文字を指定してください")
+var fields = flag.Int("f", 1, "フィールドの何番目を取り出すか指定してください")
+
 // go-cutコマンドを実装しよう
 func main() {
+	flag.Parse()
+	if flag.NArg() == 0 {
+		fmt.Fprintln(os.Stderr, "ファイルパスを指定してください。")
+		os.Exit(1)
+	}
+	fp, err := os.Open(flag.Args()[0])
+	if err != nil {
+		// Openエラー処理
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(2)
+	}
+	defer fp.Close()
 
+	scanner := bufio.NewScanner(fp)
+
+	for scanner.Scan() {
+		slice := strings.Split(scanner.Text(), *delimiter)
+		for i, str := range slice {
+			if i == (*fields - 1) {
+				fmt.Println(str)
+			}
+			i++
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/stretchr/testify v1.4.0
+	golang.org/x/tools v0.0.0-20200116165751-0508ad4c83ab // indirect
 	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543
 )
